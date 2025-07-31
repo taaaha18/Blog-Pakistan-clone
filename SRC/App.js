4964f77d818221139126d920480f4f8fbf4e2033
Taha Tayyab <125254492+taaaha18@users.noreply.github.com>
2025-07-31 23:43:51 +0500

    Add files via upload

---
+++ SRC/App.js
@@ -0,0 +1,23 @@
+// src/App.js
+import React from 'react';
+import HeroSection from './Components/heroSection';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        
+      <HeroSection />
+      <div className="main-content">
+        
+       
+      </div>
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
